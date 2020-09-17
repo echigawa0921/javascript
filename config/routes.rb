95fce119e5a58c01b979f5ddb3c01a631a95482c
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-09-17 23:00:51 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'purlldownapp/index'
+  root to: "purlldownapp#index"
 end
