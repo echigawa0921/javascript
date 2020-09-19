8b6bd402fc6d4cfb1e9fc2705b790762977e3e13
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-09-19 13:10:58 +0900

    プルダウンメニュー追記

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,26 +2,20 @@ window.addEventListener('load', function(){
 
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
-  const pullDownChild = document.querySelectorAll(".pull-down-list")
-  const currentList = document.getElementById("current-list")
 
   pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color:#FFBEDA;");
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;");
+    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
   })
 
   pullDownButton.addEventListener('click', function() {
-    if (pullDownParents.getAttribute("style") == "display:block;") {
-      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
-      pullDownParents.removeAttribute("style", "display:block;");
-    } else {
-      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
-      pullDownParents.setAttribute("style", "display:block;");
-    }
+    pullDownParents.setAttribute("style", "display:block;")
+    console.log("click OK")
   })
+})
 
   pullDownChild.forEach(function(list) {
     list.addEventListener("click", function() {
@@ -29,5 +23,3 @@ window.addEventListener('load', function(){
       currentList.innerHTML = value
     })
   })
-
-})
