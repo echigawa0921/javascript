c863ca6023b6505cc13f4dbbc4fc8a4557c76ec2
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-09-19 13:26:09 +0900

    debugger

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -12,6 +12,7 @@ window.addEventListener('load', function(){
     })
   
     pullDownButton.addEventListener('click', function() {
+      debugger
       // プルダウンメニューの表示と非表示の設定
       if (pullDownParents.getAttribute("style") == "display:block;") {
         // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
@@ -20,11 +21,5 @@ window.addEventListener('load', function(){
         // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
         pullDownParents.setAttribute("style", "display:block;")
       }
-    })
-  })
-  pullDownChild.forEach(function(list) {
-    list.addEventListener("click", function() {
-      value = list.innerHTML
-      currentList.innerHTML = value
     })
   })
